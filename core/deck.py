3f75f47bdadf0add4c8b29759c002288bced928a
yossi lerer <ylerer770@gmail.com>
2025-10-30 11:10:56 +0200

    add shuffle_by_suit

--- core/deck.py
+++ core/deck.py
@@ -7,14 +7,36 @@ def build_standard_deck() -> list[dict]:
 
     for s in suit:
         for r in ranck:
-            deck.append({r : s})
+            deck.append({"rank" : r, "suit" : s})
 
     return deck
 
 
 def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
 
-    random_number = random.randint(1, 52)
+    for i in range(5000):
+        while True:
+            random_number_1 = random.randint(0, 51)
+            random_number_2 = random.randint(0, 51)
+            if random_number_1 != random_number_2:
+
+                if deck[random_number_1]["suit"] == "H":
+                    if random_number_2 % 5 == 0:
+                        deck[random_number_1], deck[random_number_2] = deck[random_number_2], deck[random_number_1]
+                        break
+                if deck[random_number_1]["suit"] == "C":
+                    if random_number_2 % 3 == 0:
+                        deck[random_number_1], deck[random_number_2] = deck[random_number_2], deck[random_number_1]
+                        break
+                if deck[random_number_1]["suit"] == "D":
+                    if random_number_2 % 2 == 0:
+                        deck[random_number_1], deck[random_number_2] = deck[random_number_2], deck[random_number_1]
+                        break
+                if deck[random_number_1]["suit"] == "S":
+                    if random_number_2 % 7 == 0:
+                        deck[random_number_1], deck[random_number_2] = deck[random_number_2], deck[random_number_1]
+                        break
+    return deck
 
 
 
