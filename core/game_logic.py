70f22bf6b9aacce21b680b58da3f0f2d2f4bedd7
yossi lerer <ylerer770@gmail.com>
2025-10-30 11:56:51 +0200

    add dealer_play

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,5 +1,4 @@
 
-
 def calculate_hand_value(hand: list[dict]) -> int:
     hand_value = {"A": 1, "2": 2, "3": 3, "4": 4, "5" : 5, "6" : 6, "7": 7, "8": 8, "9" : 9, "10": 10, "J": 10, "Q":10, "K":10}
     sum_hans = 0
@@ -24,12 +23,21 @@ def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
     print(f"hand player: {player}, hand dealer: {dealer}.")
 
 
-
-
-
-
-
-
-
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
-    pass
+
+    sum_diler = calculate_hand_value(dealer["hand"])
+    while True:
+        if not sum_diler <= 17:
+            card1 = deck.pop(0)
+            sum_diler += card1
+        break
+    if sum_diler > 21:
+        print("Disqualified")
+        return False
+    else:
+        print("The dealer has finished his turn. ")
+        return True
+
+
+
+
