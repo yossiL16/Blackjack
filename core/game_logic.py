e22f2604ade82635c65f9858d9a8a7ce1d155f75
yossi lerer <ylerer770@gmail.com>
2025-10-30 11:29:19 +0200

    add calculate_hand_value

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,10 +1,17 @@
 
 
 def calculate_hand_value(hand: list[dict]) -> int:
-    pass
+    hand_value = {"A": 1, "2": 2, "3": 3, "4": 4, "5" : 5, "6" : 6, "7": 7, "8": 8, "9" : 9, "10": 10, "J": 10, "Q":10, "K":10}
+    sum_hans = 0
+    for i in hand:
+        value = i["rank"]
+        sum_hans += hand_value[value]
+    return sum_hans
+
 
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
-    pass
+
+
 
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
     pass
