097cdc5628e58edd20ad2e3593078477cbac9c72
yossi lerer <ylerer770@gmail.com>
2025-10-30 09:40:14 +0200

    add build_standard_deck

--- core/deck.py
+++ core/deck.py
@@ -1,10 +1,20 @@
-
+import random
 
 def build_standard_deck() -> list[dict]:
-    pass
+    ranck = ["A", "2", "3", "4", "5", "6", "7" ,"8" ,"9" ,"10", "J", "Q", "K"]
+    suit = ["H", "C", "D", "S"]
+    deck = []
+
+    for s in suit:
+        for r in ranck:
+            deck.append({r : s})
+
+    return deck
 
 
 def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
-    pass
+
+    random_number = random.randint(1, 52)
+
 
 
