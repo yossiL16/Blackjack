1a63776d0c63786293ffd6dba39d42f5f062e0ba
yossi lerer <ylerer770@gmail.com>
2025-10-30 12:40:53 +0200

    add run_full_game

--- core/player_io.py
+++ core/player_io.py
@@ -1,4 +1,6 @@
-
+from core.game_logic import calculate_hand_value
+from core.game_logic import deal_two_each
+from core.game_logic import dealer_play
 
 def ask_player_action() -> str:
 
@@ -11,3 +13,43 @@ def ask_player_action() -> str:
             continue
 
 def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
+    deal_two_each(deck, player, dealer)
+    while True:
+        chois_player = ask_player_action()
+        if chois_player == "H":
+            card = deck.pop(0)
+            player.update(card)
+            sum_player = calculate_hand_value(player["hand"])
+            if sum_player > 21:
+                print("The player is disqualified.")
+                print("The dealer won!!!")
+                print("The game is over!")
+                break
+            else:
+                continue
+        elif chois_player == "S":
+            resolt_dealer = dealer_play(deck, dealer)
+            if resolt_dealer == False:
+                print("The dealer is disqualified.")
+                print("The player won!!!")
+                print("The game is over!")
+                break
+            else:
+                hand_player = calculate_hand_value(player["hand"])
+                hand_dealer = calculate_hand_value(dealer["hand"])
+                if hand_player > hand_dealer:
+                    print("The dealer won!!!")
+                    print("The game is over!")
+                    break
+                elif hand_player < hand_dealer:
+                    print("The player won!!!")
+                    print("The game is over!")
+                    break
+                else:
+                    print("Equality")
+                    break
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from core.deck import *
+from core.game_logic import *
+from core.player_io import *
 
 if __name__ == "__main__":
-    pass
